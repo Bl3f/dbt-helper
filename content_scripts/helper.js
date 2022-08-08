c59105e25fb99bcaded561b27a3ab1bdb9fbd84e
Christophe Blefari <christophe.blefari@gmail.com>
2022-08-08 12:07:12 +0200

    Working with temp local storage and reworked the interaction between the sidebar and the content_script

--- content_scripts/helper.js
+++ content_scripts/helper.js
@@ -1,6 +1,16 @@
 let models;
 
-browser.runtime.onMessage.addListener((message) => { models = message });
+browser.runtime.onMessage.addListener((message) => {
+    console.log(`Got message ${message}`);
+});
+
+browser.storage.local.get("state").then(state => {
+    models = state.state.models;
+});
+
+browser.storage.onChanged.addListener((changes) => {
+    models = changes.state.newValue.models;
+})
 
 window.addEventListener('copy', (event) => {
     const selectionText = event.clipboardData.getData('text/plain');
--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,28 +1,47 @@
-function displaydbtInfo(manifest) {
-    document.getElementById("step-2").style.display = "block";
+const MODEL = 'model', SOURCE = 'source';
 
+function displaydbtInfo(state, nbSources, nbNodes) {
+    const {models, info} = state.state;
+
+    document.getElementById("step-2").style.display = "block";
+    document.getElementById("step-3").style.display = "block";
+
+    document.getElementById("manifestUploadedAt").innerText = info.modifiedAt;
+    document.getElementById("nbSources").innerText = nbSources || Object.entries(models).filter(([key, model]) => model.resource_type === SOURCE).length;
+    document.getElementById("nbNodes").innerText = nbNodes || Object.entries(models).filter(([key, model]) => model.resource_type === MODEL).length;
+
+    browser.tabs.query({currentWindow: true}, function (tabs){
+        tabs.forEach(tab => browser.tabs.sendMessage(tab.id, "hello"))
+    });
+}
+
+function handleManifest(manifest) {
     const sources = manifest.sources;
     const nodes = manifest.nodes;
 
-    console.log(manifest);
+    const models = {...sources, ...nodes};
+    const info = {
+        modifiedAt: new Date()
+    }
+    const state = {
+        models,
+        info,
+    }
 
-    document.getElementById("nb-sources").innerText = Object.entries(sources).length;
-    document.getElementById("nb-nodes").innerText = Object.entries(nodes).length;
+    browser.storage.local.set({state});
 
-    browser.tabs.query({currentWindow: true, active: true}, function (tabs){
-        const activeTab = tabs[0];
-        browser.tabs.sendMessage(activeTab.id, {...sources, ...nodes});
-    });
+    displaydbtInfo({state});
 }
 
 function handleFile(e) {
     e.preventDefault();
     const file = this.files[0];
     const reader = new FileReader();
-    reader.onload = () => { displaydbtInfo(JSON.parse(reader.result)) };
+    reader.onload = () => { handleManifest(JSON.parse(reader.result)); };
     reader.readAsText(file);
 }
 
 document.getElementById("uploadFile").addEventListener("change", handleFile, false);
-
-
+console.log("local storage", browser.storage.local.get("state").then(state => {
+    if (state && Object.keys(state).length) displaydbtInfo(state);
+}));
